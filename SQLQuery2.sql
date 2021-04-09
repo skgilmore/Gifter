4b98d632f51b9964cb244589c4984f4b3ed65c15
skgilmore <skg.gilmore@gmail.com>
2021-04-09 10:14:59 -0500

    Add project files.

---
+++ SQLQuery2.sql
@@ -0,0 +1,9 @@
+﻿              SELECT p.Title, p.Caption, p.DateCreated, p.ImageUrl, p.id,
+                                     p.UserProfileId,
+
+                       up.Name, up.Bio, up.Email, up.DateCreated AS UserProfileDateCreated,
+                       up.ImageUrl AS UserProfileImageUrl, up.Id
+
+                            FROM Post p
+                             LEFT JOIN UserProfile up ON p.UserProfileId= up.id
+                          WHERE p.id = 1
